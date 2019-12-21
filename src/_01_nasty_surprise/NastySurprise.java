1b69e674f4da099474c775aa4ad4eaaf16ddbeae
League Student <league@iMac-14.attlocal.net>
2019-12-20 16:41:28 -0800

    module 4
    :smile:

---
+++ src/_01_nasty_surprise/NastySurprise.java
@@ -0,0 +1,55 @@
+package _01_nasty_surprise;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class NastySurprise implements ActionListener {
+	JButton b=new JButton();
+	JButton b1=new JButton();
+	public static void main(String[] args) {
+		NastySurprise n=new NastySurprise();
+		n.createUI();
+	}
+	void createUI() {
+		JFrame f=new JFrame();
+		JPanel p=new JPanel();
+		f.add(p);
+		p.add(b);
+		p.add(b1);
+		f.pack();
+		b.setText("Trick");
+		b1.setText("Treat");
+		b.addActionListener(this);
+		b1.addActionListener(this);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource()==b) {
+			
+		}
+	}
+	private void showPictureFromTheInternet(String imageUrl) {
+	     try {
+	          URL url = new URL(imageUrl);
+	          Icon icon = new ImageIcon(url);
+	          JLabel imageLabel = new JLabel(icon);
+	          JFrame frame = new JFrame();
+	          frame.add(imageLabel);
+	          frame.setVisible(true);
+	          frame.pack();
+	     } catch (MalformedURLException e) {
+	          e.printStackTrace();
+	     }
+	}
+}
